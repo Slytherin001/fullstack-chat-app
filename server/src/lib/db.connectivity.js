8ce06570eac978712dbecfd756412f3639973e47
Slytherin001 <alimdkaif7@gmail.com>
2024-12-24 02:12:35 +0530

    changes in db

--- server/src/lib/db.connectivity.js
+++ server/src/lib/db.connectivity.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
-  const MongoURI = `mongodb+srv://kasproslytherin:ur14scEfMv6DCwvv@cluster0.sfcus.mongodb.net/chat_application
-database?retryWrites=true&w=majority&appName=Cluster0`; 
+  const MongoURI = process.env.MONGODB_URI; 
   try {
     const conn = await mongoose.connect(MongoURI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
