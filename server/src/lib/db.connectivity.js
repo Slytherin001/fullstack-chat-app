34f50c1860ceafadc34bf1f3cbe3f1cc5b0434f6
Slytherin001 <alimdkaif7@gmail.com>
2024-12-28 17:04:44 +0530

    update

--- server/src/lib/db.connectivity.js
+++ server/src/lib/db.connectivity.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 export const connectToDatabase = async () => {
   const MongoURI = process.env.MONGODB_URI; 
   try {
-    const conn = await mongoose.connect(MongoURI);
+    const conn = await mongoose.connect(MongoURI,{ useNewUrlParser: true, useUnifiedTopology: true });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("Something went wrong", error);
