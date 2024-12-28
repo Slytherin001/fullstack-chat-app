e8b3853d505ff67f6afbfa7d179026e3869adbf9
Slytherin001 <alimdkaif7@gmail.com>
2024-12-28 17:30:40 +0530

    changes in env file

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-const __dirname=path.resolve()
+const __dirname=path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
